9a31462f1f9989d9dc3981840d997dd43c46faa4
psobieranski <psobieranski@mac-mir.home>
2020-10-14 13:31:54 +0200

    added one more screen capture

--- src/test/java/ai/makeitright/tests/jobs/checkdetailsoftrigger/CheckDetailsOfTriggerTest.java
+++ src/test/java/ai/makeitright/tests/jobs/checkdetailsoftrigger/CheckDetailsOfTriggerTest.java
@@ -66,6 +66,7 @@ public class CheckDetailsOfTriggerTest extends DriverConfig {
         DisplayedTriggers displayedTriggers = schedulePage.getTriggersTable().getTriggersRowData(triggerID);
         Assertions.assertNotNull(displayedTriggers, "There is no trigger with ID: '" + triggerID + "'");
 
+        Main.report.logInfoWithScreenCapture(Methods.getScreenShotAsBase64(driver));
         Assertions.assertEquals(workflowName, displayedTriggers.getWorkflowName(),
                 "The name of the trigger's workflow is not right: " + displayedTriggers.getWorkflowName());
         Main.report.logPass("Trigger's workflow name has right value: " + workflowName);
@@ -112,6 +113,7 @@ public class CheckDetailsOfTriggerTest extends DriverConfig {
         Main.report.logPass("The 'Next run' displays the correct date: " + nextRun);
 
         TriggerDetailsPage triggerDetailsPage = schedulePage.clickFoundTrigger(triggerID);
+        Main.report.logInfoWithScreenCapture(Methods.getScreenShotAsBase64(driver));
         Assertions.assertEquals(triggerID, triggerDetailsPage.getTriggerID(),
                 "The value of TRIGGER ID is incorrect: " + triggerDetailsPage.getTriggerID());
         Main.report.logPass("In the trigger details there is a correct trigger ID value displayed: " + triggerID);
