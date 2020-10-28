6d32a364e32b841ccd19d4b0350f0837f6410611
Katarzyna Raczkowska <katarzyna.raczkowska@makeitright.ai>
2020-10-28 11:00:44 +0100

    change taskname again

--- src/test/java/ai/makeitright/tests/jobs/createnewtriggerjob/CreateNewTriggerJobTest.java
+++ src/test/java/ai/makeitright/tests/jobs/createnewtriggerjob/CreateNewTriggerJobTest.java
@@ -165,7 +165,7 @@ public class CreateNewTriggerJobTest extends DriverConfig {
     @After
     public void prepareJson() {
         JSONObject obj = new JSONObject();
-        obj.put("taskname", " Create a new job with a trigger");
+        obj.put("taskname", "Create a new job with a trigger");
         obj.put("workflowName", workflowName);
         obj.put("triggerID", triggerID);
         obj.put("executionFrequency", executionFrequency);
