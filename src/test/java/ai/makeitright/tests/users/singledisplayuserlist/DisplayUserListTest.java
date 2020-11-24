cc84b37c454fafc9d7408b234b7ee8b234e9bd2a
Katarzyna Raczkowska <katarzyna.raczkowska@makeitright.ai>
2020-11-24 13:49:06 +0100

    TC Display user list

--- src/test/java/ai/makeitright/tests/users/singledisplayuserlist/DisplayUserListTest.java
+++ src/test/java/ai/makeitright/tests/users/singledisplayuserlist/DisplayUserListTest.java
@@ -1,4 +1,55 @@
 package ai.makeitright.tests.users.singledisplayuserlist;
 
-public class DisplayUserListTest {
+import ai.makeitright.pages.common.LeftMenu;
+import ai.makeitright.pages.login.LoginPage;
+import ai.makeitright.pages.users.UsersPage;
+import ai.makeitright.utilities.DriverConfig;
+import ai.makeitright.utilities.Main;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class DisplayUserListTest extends DriverConfig {
+
+    private String pfOrganizationCardName;
+    private String pfSignInUrl;
+    private String pfUserEmail;
+    private String pfUserPassword;
+
+    @BeforeTest
+    public void before() {
+        Main.channel = System.getProperty("inputParameters.channel");
+        Main.hookUrl = System.getProperty("secretParameters.hookUrl");
+        String pfGlossary = System.getProperty("inputParameters.pfGlossary");
+        pfOrganizationCardName = System.getProperty("inputParameters.pfOrganizationCardName");
+        pfSignInUrl = System.getProperty("inputParameters.pfSignInUrl");
+        Main.pfSignInUrl = this.pfSignInUrl;
+        pfUserEmail = System.getProperty("inputParameters.pfUserEmail");
+        pfUserPassword = System.getProperty("secretParameters.pfUserPassword");
+        Main.slackFlag = System.getProperty("inputParameters.slackFlag");
+        Main.taskname = pfGlossary + ": TC - Users - Display user list [P20Ct-4]";
+    }
+
+    @Test
+    public void displayUserList() {
+        driver.get(pfSignInUrl);
+        LoginPage loginPage = new LoginPage(driver, pfSignInUrl, pfOrganizationCardName);
+        loginPage
+                .setEmailInput(pfUserEmail)
+                .setPasswordInput(pfUserPassword);
+        LeftMenu leftMenu = loginPage.clickSignInButton();
+        leftMenu.openPageBy("Users");
+
+        UsersPage usersPage = new UsersPage(driver);
+        Assert.assertTrue(usersPage.checkButtonInviteNewUserIsEnabled(), "Button 'Invite new user' should be visible and enable!");
+        Main.report.logPass("Button 'Invite new user' is visible and enabled");
+
+        Assert.assertTrue(usersPage.isUsersTextDisplayed(), "There is not header text 'Users' on the page");
+        Main.report.logPass("On the page is header text 'Users'");
+
+        Assert.assertTrue(usersPage.isUserRowDisplayed(), "There is no visible any row with user");
+        Main.report.logPass("On the page is table with at least one user row");
+
+    }
+
 }
