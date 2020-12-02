6c17499d5da9d75f136da4f1591be22c82008724
Katarzyna Raczkowska <raczkowska.kd@gmail.com>
2020-12-02 16:55:01 +0100

    log

--- src/test/java/ai/makeitright/utilities/Methods.java
+++ src/test/java/ai/makeitright/utilities/Methods.java
@@ -22,7 +22,7 @@ public class Methods extends Main {
     public static String getFirstDayOfNextMonth() {
         LocalDate currentDate = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/YYYY");
-        Main.report.logInfo("Miesiąc:" + currentDate.getMonth().toString());
+        Main.report.logInfo("Miesiąc:" + currentDate.getMonth().toString() + "current day:" + currentDate.toString());
 //        if (currentDate.getMonth().toString().equals("DECEMBER")) {
 //            return formatter.format(currentDate.plusYears(1).plusMonths(1).withDayOfMonth(1));
 //        } else {
